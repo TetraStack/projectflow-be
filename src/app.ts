0744fe08dfe38f5b68c7e23445c271ad6fefae44
Ajay Patel <patel.ajay745@gmail.com>
2025-04-14 00:52:56 -0400

    completed all the models

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
 
-
 const app: Express = express()
 
+//import routes here
+import { router as HealthCheckRouter } from "@/routes/healthCheck"
 
+app.use("/api/v1/healthCheck", HealthCheckRouter)
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,6 @@ import app from './app';
 app.use(express.json())
 const PORT = env.PORT ?? 8000
 
-//import routes here
-import { router as HealthCheckRouter } from "@/routes/healthCheck"
-
-app.use("/api/v1/healthCheck", HealthCheckRouter)
-
 connectDB().then(() => app.listen(PORT, () => {
     console.log(`server running on ${env.BASEURL}:${PORT}`)
 }))
